377d53f1c4cbe3c8297a10718156f503714ba75f
abbyhummert <abbyhummert@gmail.com>
2025-11-12 13:03:20 -0500

    Added event post page

---
+++ team1-sparkbytes/app/post/page.tsx
@@ -0,0 +1,136 @@
+'use client';
+import {Form, Input, Button, Select, Card, Typography, DatePicker, TimePicker} from 'antd';
+import {useState} from 'react';
+import { useRouter } from "next/navigation";
+import dayjs from 'dayjs';
+
+
+export default function NewEvent() {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const handleFinish = async (values: any) => {
+        setLoading(true);
+        //add logic to add data to database and handle form submission...
+    }
+
+    const addressRegex = /^[0-9A-Za-z\s.,'-]+,\s*[A-Za-z\s]+,\s*[A-Za-z]{2}\s*\d{5}(-\d{4})?$/;
+
+    return (
+        <div>
+            <Card>
+                <Typography.Title level={3}>
+                    Post a New Event
+                </Typography.Title>
+                <Form layout="vertical" onFinish={handleFinish}>
+                    <Form.Item
+                        label="Event Title"
+                        name="title"
+                        rules={[{required: true, message: 'Enter a title'}]}
+                    >
+                        <Input placeholder="Enter a title"/>
+                    </Form.Item>  
+
+                    <Form.Item
+                        label="Category"
+                        name="category"
+                        rules={[{required: true, message: "Select a category"}]}              
+                    >
+                        <Select
+                            placeholder="Select a Category"
+                            options={[
+                                {label: 'Pizza', value: 'Pizza'},
+                                {label: 'Breakfast', value: 'Breakfast'},
+                                {label: 'Dessert', value: 'Dessert'},
+                                {label: 'Otther', value: 'Other'}
+                            ]}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Description"
+                        name="description"
+                        rules={[{required: true, message: 'Add a short description'}]}
+                    >
+                        <Input.TextArea rows={3} placeholder="Describe your event..."/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Area of campus"
+                        name="area"
+                        rules={[{required:true, message: "Select an area of campus"}]}
+                    >
+                        <Select
+                            placeholder="Select an area"
+                            options={[
+                                {label: "East Campus", value: "East Campus"},
+                                {label: "West Campus", value: "West Campus"},
+                                {label: "South Campus", value: "South Campus"},
+                                {label: "Central Campus", value: "Central Campus"},
+                                {label: "Fenway Campus", value: "Fenway Campus"},
+                                {label: "Medical Campus", value: "Medical Campus"}
+                            ]}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Street Address, City, State, Zip Code"
+                        name="address"
+                        rules={[{required: true, message: "Enter a full address"},
+                            {pattern: addressRegex, message: "Enter a valid format: 700 Commonwealth Ave, Boston, MA 02215"}
+                        ]}
+                    >
+                        <Input placeholder="e.g. 700 Commonwealth Ave, Boston, MA 02215"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Building Name (optional)"
+                        name="building">
+                        <Input placeholder="e.g. Warren Towers"/>
+                    </Form.Item>
+
+                    <Form.Item label="Room Number (optional)" name="room">
+                        <Input placeholder="e.g. 511, B12, 201"/>
+                    </Form.Item>
+
+                    <div>
+                    <Form.Item
+                        label="Date"
+                        name="date"
+                        rules={[{required: true, message: "Select a date"}]}
+                    >
+                        <DatePicker
+                            format="MM/DD/YYYY"
+                            disabledDate={(current) => current && current < dayjs().startOf('day')}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Time"
+                        name="time"
+                        rules={[{required: true, message: "Select a time"}]}
+                    >
+                        <TimePicker format="h:mm A" use12Hours minuteStep={5} />
+                    </Form.Item>
+                    </div>
+
+                    <div>
+                        <Button
+                            onClick={() => router.push('/')}
+                            disabled={loading}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            type="primary"
+                            loading={loading}
+                            htmlType="submit"
+                        >
+                            Post Event
+                        </Button>
+                    </div>
+                </Form>
+            </Card>
+        </div>
+    );
+}
