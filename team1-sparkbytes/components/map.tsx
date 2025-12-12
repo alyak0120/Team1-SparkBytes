d5e53239089b9bc0837d96f1e91a914c00aa349f
abbyhummert <abbyhummert@gmail.com>
2025-12-11 23:52:57 -0500

    trying to fix for deployment

--- team1-sparkbytes/components/map.tsx
+++ team1-sparkbytes/components/map.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import {LatLngExpression} from "leaflet";
+
 
 export default function Map() {
-  const center: [number, number] = [42.3505, -71.1054];
+  const center: LatLngExpression = [42.3505, -71.1054]; // BU campus center
 
   return (
     <div style={{ height: "70vh", width: "100%", marginTop: 20 }}>
@@ -12,7 +14,12 @@ export default function Map() {
         center={center}
         zoom={15}
         scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}
+        style={{
+          height: "100%",
+          width: "100%",
+          borderRadius: 12,
+          overflow: "hidden",
+        }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -21,4 +28,3 @@ export default function Map() {
     </div>
   );
 }
-
