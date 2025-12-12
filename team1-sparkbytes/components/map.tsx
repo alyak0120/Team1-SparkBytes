230e889068741a2566efda92d89cfd8ce8690b32
Alex Kharenko <alexkharenko@crc-dot1x-nat-10-239-215-241.bu.edu>
2025-12-11 23:27:49 -0500

    vercel error

--- team1-sparkbytes/components/map.tsx
+++ team1-sparkbytes/components/map.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default function Map() {
-  const center: [number, number] = [42.3505, -71.1054]; // BU campus center
+  const center: [number, number] = [42.3505, -71.1054];
 
   return (
     <div style={{ height: "70vh", width: "100%", marginTop: 20 }}>
@@ -12,12 +12,7 @@ export default function Map() {
         center={center}
         zoom={15}
         scrollWheelZoom={false}
-        style={{
-          height: "100%",
-          width: "100%",
-          borderRadius: 12,
-          overflow: "hidden",
-        }}
+        style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -26,3 +21,4 @@ export default function Map() {
     </div>
   );
 }
+
